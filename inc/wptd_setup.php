26216dfb210d4122041ffccd117d60e68105b092
Joscha Holzhäuer <jh@dreiqbik.de>
2019-02-06 15:59:44 +0100

    TASK: Menus > register multiple menus now possible

--- inc/wptd_setup.php
+++ inc/wptd_setup.php
@@ -5,7 +5,7 @@
  *  > ADD_EXTRA_IMAGE_THUMBS
  *  > ADD_NEW_USER_ROLE
  *  > REMOVE_HTML_COMMENTS
- *  >
+ *  > REGISTER_MENUS
  *  >
  *
  * @package wptd
@@ -74,3 +74,15 @@ add_action('wp_footer', 'wptd_remove_wp_embed');
  *************************************************************************************************/
 remove_action('wp_head', 'print_emoji_detection_script', 7);
 remove_action('wp_print_styles', 'print_emoji_styles');
+
+
+/************************************************************************************************
+ * REGISTER_MENUS
+ *************************************************************************************************/
+function wptd_register_menus() {
+   register_nav_menus( array(
+      'primary' => __( 'Hauptnavigation', 'wptd' ),
+      'footer' => __( 'Footer', 'wptd' ),
+   ));
+}
+add_action( 'after_setup_theme', 'wptd_register_menus' );
