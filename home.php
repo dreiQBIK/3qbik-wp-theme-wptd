e16918804aa9b431686255ea025cae013fa9cb98
joschaholzhaeuer <jh@dreiqbik.de>
2019-05-03 17:22:41 +0200

    WIP: Blog > home page added

---
+++ home.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The main template file
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists
+ *
+ * Methods for TimberHelper can be found in the /lib sub-directory
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since   Timber 0.1
+ */
+
+$context = Timber::context();
+$templates = array( 'views/home.twig' );
+
+$post = new TimberPost();
+$context['post'] = $post;
+$context['posts'] = new Timber\PostQuery();
+$context['pagination'] = Timber::get_pagination();
+$context['sidebar'] = Timber::get_sidebar('l_sidebar.php');
+
+Timber::render( $templates, $context );
