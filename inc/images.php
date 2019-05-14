48d943fe1ca3e3b14a7d24e52c245244c028100b
Lennart Schleifer <lennartschleifer@gmail.com>
2019-05-14 16:02:03 +0200

    BUGFIX: typo in fuction

--- inc/images.php
+++ inc/images.php
@@ -14,7 +14,7 @@ function wptd_setup_image_sizes() {
     add_image_size('wptd_thumb_1600', 1600, 9999);
     add_image_size('wptd_thumb_1920', 1920, 9999);
  }
- add_action( 'after_setup_theme', 'wpdt_setup_image_sizes' );
+ add_action( 'after_setup_theme', 'wptd_setup_image_sizes' );
 
  //* Add new image sizes to media selection
  function wptd_media_selection_image_sizes( $sizes ) {
